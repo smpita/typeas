04b5a53f887e86d7b079fed65cc0325b7b6e1fd6
Sean Pearce <14220530+smpita@users.noreply.github.com>
2023-10-05 10:09:04 -0700

    Use legacy faker style

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,14 +2,10 @@
 
 namespace Smpita\TypeAs\Tests;
 
+use Illuminate\Foundation\Testing\WithFaker;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
-    protected function getPackageProviders($app)
-    {
-        return [
-            //
-        ];
-    }
+    use WithFaker;
 }
