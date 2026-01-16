df04d71caa3c347082120e89b99ee31ccb00f4e6
Sean Pearce <14220530+smpita@users.noreply.github.com>
2026-01-15 20:23:13 -0800

    Test helpers

--- tests/HelpersTest.php
+++ tests/HelpersTest.php
@@ -2,6 +2,7 @@
 
 namespace Smpita\TypeAs\Tests;
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\Attributes\Test;
 use Smpita\TypeAs\TypeAs;
@@ -22,11 +23,66 @@ class HelpersTest extends TestCase
     #[Test]
     #[Group('smpita')]
     #[Group('typeas')]
-    public function test_can_use_bool_helper(): void
+    public function test_can_use_nullable_array_helper(): void
     {
-        $mixed = $this->faker->randomElement([0, 1]);
+        $mixed = $this->faker->word();
 
-        $this->assertSame(TypeAs::bool($mixed), \Smpita\TypeAs\asBool($mixed));
+        $this->assertSame(TypeAs::nullableArray($mixed), \Smpita\TypeAs\asNullableArray($mixed));
+    }
+
+    public static function truthyDataProvider(): array
+    {
+        return [
+            'truthy_1' => [1],
+            'truthy_1_0' => [1.0],
+            'truthy_1_string' => ["1"],
+            'truthy_string' => ["true"],
+            'truthy_yes' => ["yes"],
+            'truthy_on' => ["on"],
+            'falsy_0' => [0],
+            'falsy_0_0' => [0.0],
+            'falsy_0_string' => ["0"],
+            'falsy_string' => ["false"],
+            'falsy_no' => ["no"],
+            'falsy_off' => ["off"],
+            'falsy_empty' => [""],
+        ];
+    }
+
+    #[Test]
+    #[Group('smpita')]
+    #[Group('typeas')]
+    #[DataProvider('truthyDataProvider')]
+    public function test_can_use_bool_helper(mixed $truthy): void
+    {
+        $this->assertSame(TypeAs::bool($truthy), \Smpita\TypeAs\asBool($truthy));
+    }
+
+    #[Test]
+    #[Group('smpita')]
+    #[Group('typeas')]
+    #[DataProvider('truthyDataProvider')]
+    public function test_can_use_nullable_bool_helper(mixed $truthy): void
+    {
+        $this->assertSame(TypeAs::nullableBool($truthy), \Smpita\TypeAs\asNullableBool($truthy));
+    }
+
+    #[Test]
+    #[Group('smpita')]
+    #[Group('typeas')]
+    #[DataProvider('truthyDataProvider')]
+    public function test_can_use_filter_bool_helper(mixed $truthy): void
+    {
+        $this->assertSame(TypeAs::filterBool($truthy), \Smpita\TypeAs\asFilterBool($truthy));
+    }
+
+    #[Test]
+    #[Group('smpita')]
+    #[Group('typeas')]
+    #[DataProvider('truthyDataProvider')]
+    public function test_can_use_nullable_filter_bool_helper(mixed $truthy): void
+    {
+        $this->assertSame(TypeAs::nullableFilterBool($truthy), \Smpita\TypeAs\asNullableFilterBool($truthy));
     }
 
     #[Test]
@@ -42,46 +98,6 @@ class HelpersTest extends TestCase
     #[Test]
     #[Group('smpita')]
     #[Group('typeas')]
-    public function test_can_use_float_helper(): void
-    {
-        $mixed = strval($this->faker->randomFloat());
-
-        $this->assertSame(TypeAs::float($mixed), \Smpita\TypeAs\asFloat($mixed));
-    }
-
-    #[Test]
-    #[Group('smpita')]
-    #[Group('typeas')]
-    public function test_can_use_int_helper(): void
-    {
-        $mixed = strval($this->faker->randomNumber());
-
-        $this->assertSame(TypeAs::int($mixed), \Smpita\TypeAs\asInt($mixed));
-    }
-
-    #[Test]
-    #[Group('smpita')]
-    #[Group('typeas')]
-    public function test_can_use_nullable_array_helper(): void
-    {
-        $mixed = $this->faker->word();
-
-        $this->assertSame(TypeAs::nullableArray($mixed), \Smpita\TypeAs\asNullableArray($mixed));
-    }
-
-    #[Test]
-    #[Group('smpita')]
-    #[Group('typeas')]
-    public function test_can_use_nullable_bool_helper(): void
-    {
-        $mixed = $this->faker->randomElement([0, 1]);
-
-        $this->assertSame(TypeAs::nullableBool($mixed), \Smpita\TypeAs\asNullableBool($mixed));
-    }
-
-    #[Test]
-    #[Group('smpita')]
-    #[Group('typeas')]
     public function test_can_use_nullable_class_helper(): void
     {
         $mixed = new HelperStub();
@@ -92,11 +108,31 @@ class HelpersTest extends TestCase
     #[Test]
     #[Group('smpita')]
     #[Group('typeas')]
+    public function test_can_use_float_helper(): void
+    {
+        $mixed = strval($this->faker->randomFloat());
+
+        $this->assertSame(TypeAs::float($mixed), \Smpita\TypeAs\asFloat($mixed));
+    }
+
+    #[Test]
+    #[Group('smpita')]
+    #[Group('typeas')]
     public function test_can_use_nullable_float_helper(): void
     {
         $mixed = strval($this->faker->randomFloat());
 
         $this->assertSame(TypeAs::nullableFloat($mixed), \Smpita\TypeAs\asNullableFloat($mixed));
+    }
+
+    #[Test]
+    #[Group('smpita')]
+    #[Group('typeas')]
+    public function test_can_use_int_helper(): void
+    {
+        $mixed = strval($this->faker->randomNumber());
+
+        $this->assertSame(TypeAs::int($mixed), \Smpita\TypeAs\asInt($mixed));
     }
 
     #[Test]
