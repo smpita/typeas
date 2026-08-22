e249161a49dab2a7735c70386c6070fc4e7af412
Sean Pearce <14220530+smpita@users.noreply.github.com>
2026-08-22 13:02:16 -0700

    Feature/Backed Enum Support (#95)
    * Resolve backed enums
    
    * Fix styling

---
+++ tests/Stubs/Enums/IntBackedEnumStub.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Smpita\TypeAs\Tests\Stubs\Enums;
+
+enum IntBackedEnumStub: int
+{
+    case One = 1;
+    case Two = 2;
+}
---
+++ tests/Stubs/Enums/StringBackedEnumStub.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Smpita\TypeAs\Tests\Stubs\Enums;
+
+enum StringBackedEnumStub: string
+{
+    case One = 'one';
+    case Two = 'two';
+}
